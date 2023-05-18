e5f47dc18d83797e934bcc6d42c071edd2b15163
Nejc Brelih-Wasowski <nejcbw@protonmail.com>
2023-05-18 13:44:18 +0200

    First commit.

---
+++ InertiaCoreVueApp/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using InertiaCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InertiaCoreVueApp.Controllers;
+
+[Route("/")]
+public class HomeController : Controller
+{
+
+    [HttpGet]
+    public Task<Response> Index()
+    {
+        var data = new
+        {
+            User = "John Doe",
+        };
+
+        return Task.FromResult(Inertia.Render("Index", data));
+    }
+}
---
+++ InertiaCoreVueApp/Views/App.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Views;
+
+public class App : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
