1ea3f7eaad846b1b10d88a9bedde29402663ed3e
Nejc Brelih-Wasowski <nejcbw@protonmail.com>
2024-07-06 22:09:35 +0200

    Simplified TS type declarations

--- InertiaCoreVueTS/ClientApp/src/Message.ts
+++ InertiaCoreVueTS/ClientApp/src/Message.ts
@@ -1,15 +1,3 @@
-﻿export class Message {
-    get msgText(): string {
-        return this._msgText;
-    }
-
-    set msgText(value: string) {
-        this._msgText = value;
-    }
-
-    private _msgText: string;
-
-    constructor(msgText: string = "") {
-        this._msgText = msgText;
-    }
-}
+﻿export type Message = {
+  msgText: string;
+};
--- InertiaCoreVueTS/ClientApp/src/Person.ts
+++ InertiaCoreVueTS/ClientApp/src/Person.ts
@@ -1,26 +1,4 @@
-﻿export class Person {
-    get familyName(): string {
-        return this._familyName;
-    }
-
-    set familyName(value: string) {
-        this._familyName = value;
-    }
-
-    get givenName(): string {
-        return this._givenName;
-    }
-
-    set givenName(value: string) {
-        this._givenName = value;
-    }
-
-    private _givenName: string;
-    private _familyName: string;
-
-    constructor(givenName: string = "", familyName: string = "") {
-        this._givenName = givenName;
-        this._familyName = familyName;
-    }
-
-}
+﻿export type Person = {
+  familyName: string;
+  givenName: string;
+};
